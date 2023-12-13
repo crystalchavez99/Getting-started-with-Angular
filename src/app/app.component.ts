3e4636b0ea4748199109c46d870b21e244cfb474
Crystal <crystal120599@gmail.com>
2023-12-13 14:09:48 -0800

    getting started and first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+// app.component.ts contains html template code to display
+// dev ng serve refreshes auto
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<h1>Hello Universe!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
