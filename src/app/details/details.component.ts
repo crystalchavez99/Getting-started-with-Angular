a8ea1711bf57796ff460f467bbd2089f1ac3aa8d
Crystal <crystal120599@gmail.com>
2023-12-14 10:19:00 -0800

    Create service, inject into components, and details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 // update the code to get access to url parameter
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works! {{housingLocationId}}
-    </p>
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units Available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this unit have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="listing-apply">Apply now to live here</h2>
+        <button class="primary" type="button">Apply Now</button>
+      </section>
+    </article>
   `,
   styleUrls: ['./details.component.css']
 })
@@ -19,9 +37,11 @@ export class DetailsComponent {
   // now we can get access to vital route info
   //
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor(){
-    this.housingLocationId = Number(this.route.snapshot.params["id"])
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 }
